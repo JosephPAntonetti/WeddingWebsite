6a255a578305fb4721bbc71765e7437f5a78a3d4
Joe Antonetti <me@joeantonetti.com>
2026-07-10 16:18:53 -0400

    Add Styling to Website

--- app/comingsoon/comingsoon.tsx
+++ app/comingsoon/comingsoon.tsx
@@ -1,7 +1,9 @@
+import { Typography } from "../components/Typography";
+
 export function ComingSoon() {
   return (
-    <main className="flex items-center justify-center min-h-screen">
-      <h1>Coming Soon</h1>
+    <main className="flex items-center justify-center min-h-screen bg-green-900">
+      <Typography style="header">Coming Soon</Typography>
     </main>
   );
 }
